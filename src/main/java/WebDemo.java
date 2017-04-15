fa0e8202e95f09c7305e1bd13d9dcc5773fd5c63
ke7kto <ke7kto@gmail.com>
2017-04-15 19:07:32 -0400

    Merge branch 'adriaan' of https://github.com/free1911/FreeFoodProj into adriaan
    Conflicts:
    	.idea/misc.xml
    	.idea/modules.xml
    	src/main/java/WebDemo.java

--- src/main/java/WebDemo.java
+++ src/main/java/WebDemo.java
@@ -1,28 +1,35 @@
+import com.github.scribejava.apis.TwitterApi;
+import com.github.scribejava.core.builder.ServiceBuilder;
+import com.github.scribejava.core.model.OAuth1RequestToken;
+import com.github.scribejava.core.model.OAuth1AccessToken;
+import com.github.scribejava.core.model.OAuthRequest;
+import com.github.scribejava.core.model.Verb;
+import com.github.scribejava.core.model.Response;
+import com.github.scribejava.core.oauth.OAuth10aService;
 import javafx.application.Platform;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker;
 import javafx.embed.swing.JFXPanel;
 import javafx.scene.Scene;
 import javafx.scene.web.WebEngine;
+import javafx.concurrent.Worker.State;
 import javafx.scene.web.WebView;
-
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.store.DataStoreFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
-import com.google.api.services.oauth2.Oauth2;
-import com.google.api.services.oauth2.model.Tokeninfo;
-import com.google.api.services.oauth2.model.Userinfoplus;
+import javafx.beans.value.ChangeListener;
+import javafx.stage.Stage;
 
 import javax.swing.*;
+import java.io.IOException;
 
 public class WebDemo extends JFXPanel {
     private WebView webView;
     private WebEngine webEngine;
-
+    private String authUrl;
+    final OAuth10aService service=new ServiceBuilder()
+            .apiKey("U7rWvd4nzvoOCDRQiyzUUC9o9")
+            .apiSecret("bwuFId5UkXxt8OZC3eRh5sLxzWh1LVVsWvLiJNrS1GkRQOP3yU")
+            .build(TwitterApi.instance());
+    final OAuthRequest request = new OAuthRequest(Verb.GET, "https://api.twitter.com/1.1/account/verify_credentials.json", service);
+    OAuth1RequestToken requestToken;
     public WebDemo() {
         Platform.runLater(() -> {
             initialiseJavaFXScene();
@@ -39,11 +46,40 @@ public class WebDemo extends JFXPanel {
         frame.setVisible(true);
     }
 
+    private void nextThingToDo(String textInput){
+        try{
+            final OAuth1AccessToken accessToken = service.getAccessToken(requestToken, "837125662318161920-wgN6KHBNQ60LkMHaA2Kwl53L9pkhOHy");
+            service.signRequest(accessToken, request); // the access token from step 4
+            final Response response = request.send();
+            System.out.println(response.getBody());
+        }
+        catch(java.io.IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
     private void initialiseJavaFXScene() {
         webView = new WebView();
+        try {
+            requestToken = service.getRequestToken();
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }
         webEngine = webView.getEngine();
-        webEngine.load("http://rietoverflow.com");
+        authUrl=service.getAuthorizationUrl(requestToken);
+        webEngine.load(authUrl);
+        webEngine.getOnStatusChanged();
+        Stage stage;
+        webEngine.getLoadWorker().stateProperty().addListener(
+            new ChangeListener<State>() {
+              @Override public void changed(ObservableValue ov, State oldState, State newState) {
+                  if (newState == Worker.State.SUCCEEDED) {
+                    stage.setTitle(webEngine.getLocation());
+                    System.out.println("called");
+                }
 
+                }
+            });
         Scene scene = new Scene(webView);
         setScene(scene);
     }
