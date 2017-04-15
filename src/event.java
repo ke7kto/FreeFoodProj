87466cdffb1761e0a71a1b5faf6353120ffe3f4c
free1911 <rxf1911@gmail.com>
2017-04-15 11:43:54 -0400

    web component inside a panel demo

--- src/event.java
+++ src/event.java
@@ -4,6 +4,8 @@ import javafx.scene.Scene;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
+import javax.swing.*;
+
 public class event extends JFXPanel {
     private WebView webView;
     private WebEngine webEngine;
@@ -12,6 +14,16 @@ public class event extends JFXPanel {
         Platform.runLater(() -> {
             initialiseJavaFXScene();
         });
+    }
+
+    public static void main(String[] args){
+        event e=new event();
+        JFrame frame=new JFrame();
+        frame.add(e);
+        frame.setSize(300,300);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(3);
+        frame.setVisible(true);
     }
 
     private void initialiseJavaFXScene() {
