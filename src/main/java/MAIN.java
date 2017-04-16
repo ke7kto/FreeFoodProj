bfe733abdecd1c591734e58cce928554979c5623
ke7kto <ke7kto@gmail.com>
2017-04-15 23:02:56 -0400

    Re-enabled the fb rest api for Dan
    Merge branch 'adriaan' of https://github.com/free1911/FreeFoodProj into adriaan
    
    Conflicts:
    	.idea/misc.xml
    	.idea/modules.xml
    	src/main/java/WebDemo.java

--- src/main/java/MAIN.java
+++ src/main/java/MAIN.java
@@ -1,3 +1,5 @@
+//import com.restfb.types.TestUser;
+
 /**
  * This is the main class
  * Call the methods to do the jobs
@@ -5,12 +7,16 @@
 public class MAIN {
     public static void main(String[] args){
         //data initialization
-        WebDemo.main(args);
+
+        //TwitterDemo twd = new TwitterDemo();
+
         //main usr interface
 
         //...
 
         //... Testing FBReader
+       // TestUser usr = new TestUser();
+        //System.out.println(usr.getAccessToken());
         String accessToken = "EAACEdEose0cBAOReOQl5L9QkFWeGCLBtZBQPRh9f8ZCRzHH3uA6F7fs3DjeDknPOWDCux9sOwABPX10CoNtvyjmNNK1d6T3VsQWwvbMtgeJw9wnLvNUR3nMaQDbvZBxFIB6d9GXGGcxpQZAZBibxhmzCxG2B4dojcSJx0yd7EM9fi6V16Kl0N3mfpT0ZCobOcZD";
         FBGraphReader newRder = new FBGraphReader();
         newRder.setAccessToken(accessToken);
